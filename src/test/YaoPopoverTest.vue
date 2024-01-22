4c4a94fd238ab811f565172d1a407d02e3a5bf1d
yaobisi <320130049@qq.com>
2024-01-22 10:37:49 +0800

    YaoPopover 组件新增测试用例

--- src/test/YaoPopoverTest.vue
+++ src/test/YaoPopoverTest.vue
@@ -70,6 +70,34 @@
         </template>
       </el-table-column>
     </el-table>
+    <div>其他用法</div>
+    <div>
+      <yao-popover
+          placement="top-start"
+          title="标题"
+          width="200"
+          trigger="hover"
+          content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+        <el-button slot="reference">hover 激活</el-button>
+      </yao-popover>
+      <yao-popover
+          placement="bottom"
+          title="标题"
+          width="200"
+          trigger="click"
+          content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+        <el-button slot="reference">click 激活</el-button>
+      </yao-popover>
+      <yao-popover
+          placement="bottom"
+          title="标题"
+          width="200"
+          trigger="manual"
+          content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+          v-model="visible">
+        <el-button slot="reference" @click="visible = !visible">手动激活</el-button>
+      </yao-popover>
+    </div>
   </div>
 </template>
 
@@ -81,6 +109,7 @@ export default {
   components: {YaoPopover},
   data() {
     return {
+      visible: false,
       tableData: [
         {
           date: "2016-05-02",
